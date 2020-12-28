242b6b1895426d135d85a297689a48f99296905f
Rijad Fejzic <rijad.fejzic@eestec-sa.ba>
2020-12-28 14:09:44 +0100

    Fix trening details page

--- resources/views/admin/treninzi/detalji.blade.php
+++ resources/views/admin/treninzi/detalji.blade.php
@@ -8,14 +8,51 @@
         </div>
         <div class="col-12 col-md-8 p-0 mt-5 mt-md-0 ml-md-3 border rounded border-secondary sadrzaj">
             <div class="list-group-item naslov-sadrzaja pb-0">
-                <p>Detalji treninga</p>
+                <p>Pregled treninga</p>
             </div>
             <div class="row m-2 p-1">
                 <a href="{{ route('admin.treninzi') }}" class="btn btn-sm  btn-outline-success col-12 col-sm-3">Nazad na treninge</a>
             </div>
-            <h1>{{ $trening->naziv }}</h1>
-            <p>{{ $trening->opis }}</p>
-            <img src="{{ $trening->slika }}" />
+            <div class="container-fluid">
+                <div class="row mb-3 justify-content-center">
+                    <div class="col-lg-4">
+                        <div class="card mb-3">
+                            <div class="card-body text-center shadow">
+                                <img class="mb-3 mt-4" src="{{ asset('img/rasim.png') }}" width="160" height="160"/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col">
+                            <div class="card shadow mb-3">
+                                <div class="card-header py-3">
+                                    <p class="text-primary m-0 font-weight-bold">Podaci o treningu</p>
+                                </div>
+                                <div class="card-body">
+                                    <div class="form-row">
+                                        <div class="col">
+                                            <div class="form-group">
+                                                <label for="naziv"><strong>Naziv</strong></label>
+                                                <div class="form-control">{{ $trening->naziv }}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="form-row">
+                                        <div class="col">
+                                            <div class="form-group">
+                                                <label for="opis"><strong>Opis</strong></label>
+                                                <div class="form-control">{{ $trening->opis }}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
