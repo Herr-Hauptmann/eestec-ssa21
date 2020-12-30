21f9695aecd549e0253c4eb061e69ca392d378dd
Rijad Fejzic <rijad.fejzic@eestec-sa.ba>
2020-12-30 10:45:47 +0100

    Fix edit Medij page

--- resources/views/admin/mediji/uredjivanje.blade.php
+++ resources/views/admin/mediji/uredjivanje.blade.php
@@ -2,40 +2,50 @@
 
 
 @section('content')
-    <div class="container-fluid pt-5 pl-5 izbornik">
-        <div class="row">
-            <div class="col-md-3 col-12">
-                @include('admin.izbornik')
+<div class="container-fluid pt-5 pl-5 izbornik">
+    <div class="row">
+        <div class="col-md-3 col-12">
+            @include('admin.izbornik')
+        </div>
+        <div class="col-12 col-md-8 p-0 mt-5 mt-md-0 ml-md-3 border rounded border-secondary sadrzaj">
+            <div class="list-group-item naslov-sadrzaja pb-0">
+                <p>Uredjivanje medija</p>
             </div>
-            <div class="col-12 col-md-8 p-0 mt-5 mt-md-0 ml-md-3 border rounded border-secondary sadrzaj">
-                <div class="list-group-item naslov-sadrzaja pb-0">
-                    <p>Dodavanje novi medij</p>
-                </div>
-                <div class="row m-2 p-1">
-                    <a href="{{ route('admin.mediji') }}" class="btn btn-sm  btn-outline-success col-12 col-sm-3">Nazad na medije</a>
-                </div>
-                <form class="mt-5 px-5" action="{{ route('admin.mediji.azuriranje', $medij->id) }}" method="POST">
-                    {{ csrf_field() }}
-                    <div class="form-row">
-                        <div class="form-group col-md-12">
-                            <label for="nazivMedija">Naziv</label>
-                            <input type="text" class="form-control" id="nazivMedija" placeholder="Naziv" name="naziv" value="{{$medij->naziv}}">
-                        </div>
-                        <div class="form-group col-md-12 mt-3">
-                            <label for="linkMedija">Link</label>
-                            <input type="text" class="form-control" id="linkMedija" placeholder="Link" name="link" value="{{$medij->link}}">
-                        </div>
-
-                        <div class="custom-file col-md-12 mt-5">
-                            <input type="file" class="custom-file-input" id="slikaMedija" placeholder="slika" name="slika" value="{{$medij->slika}}">
-                            <label class="custom-file-label" for="slikaMedija">Umetni sliku</label>
-                        </div>
+            <div class="row m-2 p-1">
+                <a href="{{ route('admin.mediji') }}" class="btn btn-sm  btn-outline-success col-12 col-sm-3">Nazad na medije</a>
+            </div>
+            <form class="m-5" action="{{ route('admin.mediji.azuriranje', $medij->id) }}" method="POST" enctype="multipart/form-data">
+                {{ csrf_field() }}
+                <div class="form-row">
+                    <div class="form-group col-md-6">
+                        <label for="naziv">Naziv</label>
+                        <input type="text" class="form-control" id="naziv" placeholder="Naziv" name="naziv"
+                            value="{{ old('naziv') ?? $medij->naziv }}">
+                        @if ($errors->first('naziv'))
+                            <div class="alert alert-danger">
+                                {{ $errors->first('naziv') }}
+                            </div>
+                        @endif
                     </div>
-
-                    <button type="submit" class="btn btn-primary mt-5">Spasi medij</button>
-                </form>
-            </div>
+                    <div class="form-group col-md-6">
+                        <label for="link">Link</label>
+                        <input type="text" class="form-control" id="link" placeholder="Link" name="link"
+                            value="{{ old('link') ?? $medij->link }}">
+                        @if ($errors->first('link'))
+                            <div class="alert alert-danger">
+                                {{ $errors->first('link') }}
+                            </div>
+                        @endif
+                    </div>
+                    <div class="custom-file col-md-6 mt-4">
+                        <input type="file" class="custom-file-input" id="slika" name="slika">
+                        <label class="custom-file-label" for="slika" name="slika">Umetni sliku</label>
+                    </div>
+                </div>
+                <button type="submit" class="btn btn-primary mt-5">Spasi medij</button>
+            </form>
         </div>
     </div>
+</div>
 
 @endsection
